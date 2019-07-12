0268815396994a1d608df3ea699ef6cc8cac84aa
Gudbrandr42 <42652428+Gudbrandr42@users.noreply.github.com>
2019-07-12 11:29:39 -0400

    trying to set up email functionality on contact page.

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var contactAddress = "mark.bullard42@gmail.com";
+var queryString = require('querystring'); // provides utilities for parsing and formatting URL query strings
+
+// helps send the email for the user upon clicking submit
+var mailer = require('nodemailer').createTransport ({
+  service: "gmail",
+  auth: {
+    user: process.env.gmail_address,
+    pass: process.env.gmail_password
+  }
+});
+
+// this is where the magic happens to set up email and send it
+module.exports.contact = (event, context, callback) => {
+  var body = querystring.parse(event.body);
+  mailer.sendMail({
+    from: body.from,
+    to: [contactAddress],
+    subject: body.subject || '[No subject]',
+    html: body.message || '[No message]'
+  }, function(err, info) {
+    if (err) return callback(err);
+    callback(null, {statusCode: 200, body: "Success!"});
+  });
+});
