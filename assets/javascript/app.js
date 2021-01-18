0bb8bc888117d28a256f71a5c30a24a79d28b622
Gudbrandr42 <mark.bullard42@gmail.com>
2021-01-18 18:50:25 -0500

    Updating contact page
    Another attempt to update the behavior of the contact page so that a comment gets sent to me via email, using EmailJS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,22 @@
-var contactAddress = "mark.bullard42@gmail.com";
-var queryString = require("querystring"); // provides utilities for parsing and formatting URL query strings
+function sendEmail() {
+  emailjs.init("user_7Vs5eg6WEMfm20HJ2KwYQ");
+}
 
-// helps send the email for the user upon clicking submit
-var nodemailer = require("nodemailer").createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.gmail_address,
-    pass: process.env.gmail_password,
-  },
-});
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      //creates a five digit number for contact
+      this.contact_number.value = (Math.random() * 100000) | 0;
 
-// this is where the magic happens to set up email and send it
-module.exports.contact = (event, context, callback) => {
-  var body = querystring.parse(event.body);
-  nodemailer.sendMail(
-    {
-      from: body.from,
-      to: [contactAddress],
-      subject: body.subject || "[No subject]",
-      html: body.message || "[No message]",
-    },
-    function (err, info) {
-      if (err) return callback(err);
-      callback(null, { statusCode: 200, body: "Success!" });
-    }
-  );
+      emailjs.sendForm("contact_service", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
 };
