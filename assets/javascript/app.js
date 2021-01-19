5db26cf4451aff2a3b2208f0f8183f734cc43498
Gudbrandr42 <mark.bullard42@gmail.com>
2021-01-18 19:05:40 -0500

    reverted changes
    emailjs did not work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,27 @@
-function sendEmail() {
-  emailjs.init("user_7Vs5eg6WEMfm20HJ2KwYQ");
-}
+var contactAddress = "mark.bullard42@gmail.com";
+var queryString = require("querystring"); // provides utilities for parsing and formatting URL query strings
 
-window.onload = function () {
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      //creates a five digit number for contact
-      this.contact_number.value = (Math.random() * 100000) | 0;
+// helps send the email for the user upon clicking submit
+var nodemailer = require("nodemailer").createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.gmail_address,
+    pass: process.env.gmail_password,
+  },
+});
 
-      emailjs.sendForm("contact_service", "contact_form", this).then(
-        function () {
-          console.log("SUCCESS!");
-        },
-        function (error) {
-          console.log("FAILED...", error);
-        }
-      );
-    });
+module.exports.contact = (event, context, callback) => {
+  var body = querystring.parse(event.body);
+  nodemailer.sendMail(
+    {
+      from: body.from,
+      to: [contactAddress],
+      subject: body.subject || "[No subject]",
+      html: body.message || "[No message]",
+    },
+    function (err, info) {
+      if (err) return callback(err);
+      callback(null, { statusCode: 200, body: "Success!" });
+    }
+  );
 };
